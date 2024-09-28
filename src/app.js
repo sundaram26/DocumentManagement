56f5e1c77bcab1e40920baf7e61e74baa27b0919
Arn3092 <arnold.bkbck@gmail.com>
2024-09-29 03:00:00 +0530

    corrected session storage problem

--- src/app.js
+++ src/app.js
@@ -2,23 +2,8 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import session from 'express-session';
-// import MongoDBStore from 'connect-mongodb-session'
-// import mongoose from 'mongoose';
-// import { DB_NAME } from "./constants.js";
 
 const app = express()
-
-// Connect to MongoDB
-// const mongoDBStore = MongoDBStore(session);
-
-// const store = new mongoDBStore({
-//     uri: `${process.env.MONGODB_URI}/${DB_NAME}`, 
-//     collection: 'sessions' 
-// });
-
-// store.on('error', (error) => {
-//     console.error('MongoDB session store error:', error);
-// });
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -33,10 +18,8 @@ app.use(session({
     secret: process.env.SESSION_SECRET_KEY, 
     resave: false,
     saveUninitialized: true,
-    // store: store,
     cookie: { 
-        secure: true,
-        httpOnly: true,
+        secure: false,
         maxAge: 2* 60 * 60 * 1000 
     } // Set to true if using HTTPS
 }));
