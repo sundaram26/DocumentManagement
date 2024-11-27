440206eed57050f53bf8ac2b9534a402f12d9962
sundaram26 <singhsundaram2004@gmail.com>
2024-11-27 18:55:12 +0530

    added admin role in users model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new Schema(
         },
         role: {
             type: String,
-            enum: ["DMS", "Rotaract"],
+            enum: ["DMS", "Rotaract", "admin"],
             required: true,
         },
         email: {
